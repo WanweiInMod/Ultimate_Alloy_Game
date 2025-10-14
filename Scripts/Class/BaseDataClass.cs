4ee7b4a342fa43a85837913a407b16cde4bd8452
万维魔方 <823918122@qq.com>
2025-10-15 07:53:14 +0800

    修改：调整全局数据内容与结构

---
+++ Scripts/Class/BaseDataClass.cs
@@ -0,0 +1,15 @@
+﻿namespace Ult_Alloy_250221.Scripts.Class;
+
+// 基类
+public class BaseDataClass
+{
+    
+}
+
+// 库存类
+public class InventoryItem : BaseDataClass
+{
+    
+}
+
+// 设施类
--- Scripts/GlobalData.cs
+++ Scripts/GlobalData.cs
@@ -1,27 +1,28 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 
 public partial class GlobalData : Node
 {
     // 资源数据
-    public static Dictionary<string, int> Items = new Dictionary<string, int>()
-    {
-        { "stone", 0 },
-        { "copper", 0 },
-        { "iron", 0 },
-        { "silver", 0 },
-        { "gold", 0 },
-        { "cash", 0 },
-    };
+    public static Dictionary<string, int> Items = new Dictionary<string, int>();
     
     // 设施数据
-    public static Dictionary<string, int> Facilities = new Dictionary<string, int>()
+    public static Dictionary<string, int> Facilities = new Dictionary<string, int>();
+
+
+    public override void _Ready()
     {
-        { "furnace", 0 },
-        { "miner", 0 },
-    };
+        LoadDataFromJson();
+    }
     
+    // —————— 配置文件读取函数 ——————
+    private void LoadDataFromJson()
+    {
+        using var file = FileAccess.Open("res://Data/Level/Main.json", FileAccess.ModeFlags.Read);
+    }
+
     // —————— 资源管理函数 ——————
     public static void AddItem(string _itemName, int _amount)
     {
@@ -31,7 +32,8 @@ public partial class GlobalData : Node
         }
         else
         {
-            Items.Add(_itemName, _amount);
+            GD.PrintErr($"Item {_itemName} not found in inventory.");
+            return;
         }
 
         GD.Print($"{_amount} of {_itemName} added to inventory.");
