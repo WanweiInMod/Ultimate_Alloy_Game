8642dc7ff51f40d7658af39950fe5412cb8ece09
万维魔方 <823918122@qq.com>
2025-10-23 10:03:10 +0800

    功能：将场景提取为类，便于复用

--- Scripts/info_ui.gd
+++ Scripts/info_ui.gd
@@ -1,12 +1,20 @@
 extends Control
 
+# 定义类方便处理更新
+class_name InfoUi
 
+# 属性
+var target_item: String
+var item_icon: Texture2D
+var item_data: Node
+
+# 初始化
 func _ready() -> void:
+	# 类属性
+	
+	# 子节点属性
 	$InfoCount.text = '0'
 	return
 
 
-func _on_update_info(count:int) -> void:
-	print('Info has updated.')
-	$InfoCount.text = str(count)
-	return
+
